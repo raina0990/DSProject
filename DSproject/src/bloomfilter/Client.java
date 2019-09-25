5f63fe8da00228396fc4e3533f5825582ec46ae6
ak_0905 <akanksha0990@gmail.com>
2019-09-24 20:13:34 -0400

    added input file and made changes in client file

--- DSproject/src/bloomfilter/Client.java
+++ DSproject/src/bloomfilter/Client.java
@@ -4,13 +4,45 @@ import java.io.*;
 
 
 public class Client {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		
+		
+		FileInputStream finput  = new FileInputStream("input.txt");
+		DataInputStream input  = new DataInputStream(finput);
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(input));
+		//Calling bloom filter interface
+		
+	
 		try {
 		//Reading input from a file
+			
+			String readStr;
+			String[] splitString;
+			
+		    while ((readStr =buffer.readLine()) != null ) {
+		    	
+		    
+		    	System.out.println(readStr);
+		    	splitString = readStr.split(" ");
+		    	System.out.println(splitString[0]);
+		    // Calling add method of bloom filter remotely
+		    	if (splitString[0].contentEquals("add")) {
+
+		    			System.out.println("Calling add method of bloom filter remotely on" + splitString[1]);
+		    			
+		    	}
+		   // Calling isPresent method of bloom filter remotely
+		    	if (splitString[0].contentEquals("test")) {
+		    		
+		    	
+		    			System.out.println("Calling is method of bloom filter remotely on" + splitString[1]);
+		    			
+		    	}
+		    
+		    }//while
 		
 			
-		//Calling bloom filter interface
+		
 		
 			
 		//Generate output 
@@ -18,9 +50,11 @@ public class Client {
 			
 		}//try
 		catch(Exception e) {
-			
+			System.out.println("Exception :" + e.getMessage());
 		}//catch
 		finally {
+			
+				input.close();
 			
 		}//finally
 
