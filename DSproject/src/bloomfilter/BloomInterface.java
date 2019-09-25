afb64930a995469d3f612543cb96c946282931c3
Srujana Malisetti <43800993+SrujanaMalisetti@users.noreply.github.com>
2019-09-24 23:29:26 -0400

    Commit1

--- DSproject/src/bloomfilter/BloomInterface.java
+++ DSproject/src/bloomfilter/BloomInterface.java
@@ -1,12 +1,15 @@
 package bloomfilter;
 import java.rmi.*;
+import java.util.BitSet;
 
 public interface BloomInterface extends Remote{
+	 
+	public BitSet bloomfilter= new BitSet();
 	//Adding a string to bloom filter
-	public void add(String s);
+	public void add(String s) throws RemoteException;
 	//Check for the the presence of the string in the bloom filter
-	public Boolean isPresent(String s);
+	public Boolean isPresent(String s) throws RemoteException;
 	//reset bloom filter
-	public void reset();
+	public void reset() throws RemoteException;
 	
 }
